67ed2d15c6601b9ac8ccc39046e4f4b4f2e0719a
ReparaTec <152349963+ReparaTec@users.noreply.github.com>
2023-11-28 21:22:36 -0500

    Add files via upload

---
+++ reparaTec/src/main/java/com/reparatec/reparaTec/ReparaTecApplication.java
@@ -0,0 +1,13 @@
+package com.reparatec.reparaTec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReparaTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReparaTecApplication.class, args);
+	}
+
+}
