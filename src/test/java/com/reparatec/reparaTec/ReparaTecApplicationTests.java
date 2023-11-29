7a5c283ea060aeb019538a02a0f5e165901e93c9
ReparaTec <reparatec509@gmail.com>
2023-11-28 21:17:30 -0500

    push

---
+++ src/test/java/com/reparatec/reparaTec/ReparaTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reparatec.reparaTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReparaTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
